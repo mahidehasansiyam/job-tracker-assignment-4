9f2947e5169b4c356756a601f6c97093367152dd
Mahedi <mahidehasansiyam123@gmail.com>
2026-02-28 19:44:42 +0600

    correction

--- style.js
+++ style.js
@@ -1,112 +1,107 @@
-let noOfall = document.getElementById('no-of-all');
-let noOfInterview = document.getElementById('no-of-interview');
-let noOfRejected = document.getElementById('no-of-rejected');
-let interview = document.querySelectorAll('.interview');
-let rejected = document.querySelectorAll('.rejected');
-let delet = document.querySelectorAll('.delet');
-let cards = document.querySelector('#cards');
-let jobCount = document.querySelector('#job-count');
-let allSection = document.getElementById('allSection');
-let interviewSection = document.getElementById('interviewSection');
-let rejectedSection = document.getElementById('rejectedSection');
+
+const allSection = document.getElementById('allSection');
+const interviewSection = document.getElementById('interviewSection');
+const rejectedSection = document.getElementById('rejectedSection');
+
+const NoOfAll = document.getElementById('no-of-all');
+const NoOfIntervied = document.getElementById('no-of-interview');
+const NoOfRejected = document.getElementById('no-of-rejected');
+
+const emptyState = document.getElementById('emptyState');
+
+const jobCount = document.getElementById('job-count');
+
+
 let currentPage = 'allBtn';
-let emptyState = document.getElementById('emptyState');
 
-// Button Toggle 
 function toggleBtn(currentBtn) {
-  
-  let btns = ['allBtn', 'interviewBtn', 'rejectedBtn']; 
-   
-  currentPage = currentBtn;
 
-  for (const btn  of btns) { 
-    const btnNane = document.getElementById(btn);
-    // console.log(btnNane);
-    if (btn === currentBtn) {
-      btnNane.classList.remove('bg-gray-300');
-      btnNane.classList.add('bg-blue-800','text-white') 
+  currentPage = currentBtn;
+  const btns = ['allBtn', 'interviewBtn', 'rejectedBtn'];
+  for (const btn of btns) {
+
+    const button = document.getElementById(btn)
+    button.classList.remove('bg-black','text-white')
+
+    if (currentBtn === btn) {
+      button.classList.add('bg-black', 'text-white');
     }
-    else {
-      btnNane.classList.add('bg-gray-300');
-      btnNane.classList.remove('bg-blue-800', 'text-white');
+  }
+  
+  emptyState.classList.add('hidden');
+
+  const sections = [allSection, interviewSection, rejectedSection]
+  for (const section of sections) {
+    section.classList.add('hidden')
+
+    if (currentBtn === 'allBtn') {
+      allSection.classList.remove('hidden')
+      jobCount.innerText = allSection.children.length;
       
     }
-  } 
-
-  emptyState.classList.add('hidden')
-  let sections = [allSection, interviewSection, rejectedSection];
-  for (const section of sections) {
-    section.classList.add('hidden');
-  } 
-
-  if (currentBtn === 'allBtn') { 
-    allSection.classList.remove('hidden');  
-    if (allSection.children.length < 1) {
-      emptyState.classList.remove('hidden');
+    if (currentBtn === 'interviewBtn') {
+      interviewSection.classList.remove('hidden');
+      jobCount.innerText = interviewSection.children.length;
+    }
+    if (currentBtn === 'rejectedBtn') {
+      rejectedSection.classList.remove('hidden');
+      jobCount.innerText = rejectedSection.children.length;
     }
   }
-  else if (currentBtn === 'interviewBtn') {
-    interviewSection.classList.remove('hidden');
-    if (interviewSection.children.length < 1) {
-      emptyState.classList.remove('hidden');
-    }
-  }
-  else if (currentBtn === 'rejectedBtn') {
-    rejectedSection.classList.remove('hidden');
-    if (rejectedSection.children.length < 1) {
-      emptyState.classList.remove('hidden');
-    }
-  }
-  countUpdate();
-} 
-
+  EmptyState();
+}
 toggleBtn(currentPage);
 
-document.getElementById('sections').addEventListener('click', function (e) {
-  let parentCard = e.target.closest('.card');
-  let fullContent = parentCard.parentNode;
-  const status = document.querySelector('.status');
 
+document.getElementById('sections').addEventListener('click', (e) => {
+  
+  const parentCard = e.target.closest('.card');
+  const fullCardContent = parentCard.parentNode;
+
+  const status = parentCard.querySelector('.status')
+  
+  // console.log(e.target);
   if (e.target.classList.contains('interview')) {
     status.innerText = 'Interview';
-    interviewSection.appendChild(parentCard)
+    interviewSection.appendChild(parentCard);
   }
   if (e.target.classList.contains('rejected')) {
     status.innerText = 'Rejected';
     rejectedSection.appendChild(parentCard);
   }
   if (e.target.classList.contains('delet')) {
-    fullContent.removeChild(parentCard);
+    status.innerText = 'NOT Applied';
     allSection.appendChild(parentCard);
   }
+
   
   countUpdate();
+  EmptyState();
 })
 
-// Count Update
-function countUpdate() {
-  noOfall.innerText = allSection.children.length;
-  noOfInterview.innerText = interviewSection.children.length;
-  noOfRejected.innerText = rejectedSection.children.length;
+function EmptyState() {
 
-  const counts = {
-    allBtn: allSection.children.length,
-    interviewBtn: interviewSection.children.length,
-    rejectedBtn: rejectedSection.children.length
-  };
+  emptyState.classList.add('hidden');
 
-  noOfall.innerText = counts.allBtn;
-  noOfInterview.innerText = counts.interviewBtn;
-  noOfRejected.innerText = counts.rejectedBtn;
-
-  jobCount.innerText = counts[currentPage]
-
-  if (counts[currentPage] < 1) {
+  if (currentPage === 'allBtn' && allSection.children.length < 1) {
     emptyState.classList.remove('hidden');
   }
-  else {
-    emptyState.classList.add('hidden');
+  if (currentPage === 'interviewBtn' && interviewSection.children.length < 1) {
+    emptyState.classList.remove('hidden');
   }
+  if (currentPage === 'rejectedBtn' && rejectedSection.children.length < 1) {
+    emptyState.classList.remove('hidden');
+  }
+}
+EmptyState()
 
+function countUpdate(){
+  NoOfAll.innerText = allSection.children.length;
+  NoOfIntervied.innerText = interviewSection.children.length;
+  NoOfRejected.innerText = rejectedSection.children.length;
+  
 }
 countUpdate();
+
+
+
